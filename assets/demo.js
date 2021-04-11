558ee1454b5f7656a3b5a281ae6f787bd9ae06b2
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2021-04-11 17:30:50 +0200

    Display a Babylon scene in the canvas

--- assets/demo.js
+++ assets/demo.js
@@ -1,10 +1,25 @@
+const ANTI_ALIASING_ENABLED = true;
+
+function createScene(engine, canvas) {
+	const scene = new BABYLON.Scene(engine);
+	const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+	camera.attachControl(canvas, true);
+	const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+	const box = BABYLON.MeshBuilder.CreateBox("box", {});
+	return scene;
+}
+
 function main() {
 	const screen = document.getElementById("screen");
-	if (screen) {
-		alert("Fully loaded");
-	} else {
-		alert("Nope");
-	}
+	const engine = new BABYLON.Engine(screen, ANTI_ALIASING_ENABLED);
+	const scene = createScene(engine, screen);
+	engine.runRenderLoop(() => {
+		scene.render();
+	});
+
+	window.addEventListener("resize", () => {
+		engine.resize();
+	});
 }
 
 document.addEventListener("DOMContentLoaded", main);
